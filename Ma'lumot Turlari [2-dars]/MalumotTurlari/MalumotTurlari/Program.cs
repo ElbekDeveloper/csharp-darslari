38b78e3e0425011f32724e6e388dc5c706dadf52
ElbekDeveloper <e.normurodov@mail.ru>
2020-03-02 15:38:54 +0500

    add material of 2nd lesson

---
+++ Ma'lumot Turlari [2-dars]/MalumotTurlari/MalumotTurlari/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MalumotTurlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // o'zgaruvchilarni e'lon qilish
+            var birinchiSon = 1;
+            var kuchukcha = '@';
+            var meningMaqsadim = "Dasturlashni o'rganish!";
+            float kasrSon = 2.4212f;
+            double kattaKasr = 3415423.1234123412;
+            bool dasturlashYaxshi = true; // rost
+
+            //o'zgaruvchilarni konsolga chiqarish
+            Console.WriteLine(birinchiSon);
+            Console.WriteLine(kuchukcha);
+            Console.WriteLine(meningMaqsadim);
+            Console.WriteLine(kasrSon);
+            Console.WriteLine(kattaKasr);
+            Console.WriteLine(dasturlashYaxshi);
+
+           
+        }
+    }
+}
