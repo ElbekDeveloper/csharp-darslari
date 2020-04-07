3c513a5d30063aabf78651dfae162db6ecdc00bb
ElbekDeveloper <e.normurodov@mail.ru>
2020-04-07 12:07:45 +0500

    add lesson 4

---
+++ Operatorlar [3-dars]/Operatorlar/Operatorlar/Operatorlar/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 10;
+            double c = 21.12;
+            double d = 11.44;
+            bool rost = true;
+            bool yolgon = false;
+
+
+
+                       //Arifmetik Operatorlar
+                        Console.WriteLine("Arifmetik Operatorlar");
+                        Console.WriteLine(a + b); // qo'shish
+                        Console.WriteLine(a - b); // ayirish
+                        Console.WriteLine(a * b); // ko'parytirish
+                        Console.WriteLine(a / b); //bo'lish
+                        Console.WriteLine(b % a); // qoldiq
+                        Console.WriteLine(a++); // postfix
+                        Console.WriteLine(++a); // prefix
+                        Console.WriteLine(a--); // postfix
+                        Console.WriteLine(--a); // prefix
+
+                        // Qiyoslash Operatorlari
+                        // true yoki false qiymat qaytaradi
+                        Console.WriteLine("Qiyoslash operatorlari");
+                        Console.WriteLine(c == d);
+                        Console.WriteLine( c != d);
+                        Console.WriteLine( c > d);
+                        Console.WriteLine( c < d);
+
+                        //Mantiqiy operatorlar
+                        // true yoki false qiymat qaytaradi
+                        Console.WriteLine("Mantiqiy Operaotorlar");
+                        Console.WriteLine(rost && yolgon);
+                        Console.WriteLine(rost || yolgon);
+                        Console.WriteLine(!(rost && yolgon));
+
+            // Tayinlash operatorlari
+            Console.WriteLine("Tayinlash Operatorlari");
+            Console.WriteLine(a);
+            Console.WriteLine(a = b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a += b);
+            Console.WriteLine(a -= 2);
+            Console.WriteLine(a /= 2);
+            Console.WriteLine(a *= 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ if shartlari [4-dars]/IfShartlari/IfShartlari/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Ro'yhatdan o'tish sistemasi
+
+namespace IfShartlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //o'zgaruvchilarni e'lon qilamiz
+            string ism;
+            int yosh;
+
+
+            //o'zgaruchilarni qabul qilamiz
+            Console.WriteLine("Salom, iltimos quyidagi ma'lumotlarni kiriting!");
+            Console.Write("Ism: ");
+            ism = Console.ReadLine();
+            Console.Write("Yosh: ");
+            yosh = Convert.ToInt32(Console.ReadLine());
+
+            //agar yoshi kichik bo'lsa 18 dan
+            if(yosh < 18)
+            {
+                Console.WriteLine($"Kechirasiz {ism}, siz hali yoshsiz!");
+            } else if (yosh >= 18 && yosh < 27)
+            {
+                Console.WriteLine($"Tabriklaymiz {ism}, siz harbiy xizmat o'tay olasiz!");
+            }
+            else
+            {
+                Console.WriteLine("Siz harbiy xizmatga bora olmaysiz!");
+            }
+
+            //switch bilan ishlash
+            switch (ism)
+            {
+                case "Elbek":
+                    Console.WriteLine("Siz dasturlashni o'rgatasiz!");
+                    break;
+                case "G'anisher":
+                    Console.WriteLine("Siz dasturlashni o'rganasiz");
+                    break;
+                // yana "case" yozish mumkin
+                default:
+                    Console.WriteLine("Siz default holatga tushdingiz.");
+                    break;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
