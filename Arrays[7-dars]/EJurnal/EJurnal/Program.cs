ee3ce190e63fa8b3435615205488ce3874c3ab7a
ElbekDeveloper <e.normurodov@mail.ru>
2020-06-13 09:41:43 +0500

    add new lesson

--- Arrays[7-dars]/EJurnal/EJurnal/Program.cs
+++ Arrays[7-dars]/EJurnal/EJurnal/Program.cs
@@ -46,23 +46,42 @@ namespace EJurnal
                 5,
                 3
             };
-            Array.Sort(random);
-            Console.WriteLine(random[0]);
+            //Array.Sort(random);
+            //Console.WriteLine(random[0]);
 
-            Console.WriteLine(ismlar.Length);
-            Console.WriteLine(baholar.Length);
+            //Console.WriteLine(ismlar.Length);
+            //Console.WriteLine(baholar.Length);
 
-            
-            Console.WriteLine($"1. {ismlar[0]} | {baholar[0]} |");
-            Console.WriteLine($"2. {ismlar[1]} | {baholar[1]} |");
-            Console.WriteLine($"3. {ismlar[2]} | {baholar[2]} |");
-            Console.WriteLine($"4. {ismlar[3]} | {baholar[3]} |");
-            Console.WriteLine($"5. {ismlar[4]} | {baholar[4]} |");
-            Console.WriteLine($"6. {ismlar[5]} | {baholar[5]} |");
-            Console.WriteLine($"7. {ismlar[6]} | {baholar[6]} |");
-            Console.WriteLine($"8. {ismlar[7]} | {baholar[7]} |");
-            Console.WriteLine($"9. {ismlar[8]} | {baholar[8]} |");
-            Console.WriteLine($"10. {ismlar[9]} | {baholar[9]} |");
+
+            //Console.WriteLine($"1. {ismlar[0]} | {baholar[0]} |");
+            //Console.WriteLine($"2. {ismlar[1]} | {baholar[1]} |");
+            //Console.WriteLine($"3. {ismlar[2]} | {baholar[2]} |");
+            //Console.WriteLine($"4. {ismlar[3]} | {baholar[3]} |");
+            //Console.WriteLine($"5. {ismlar[4]} | {baholar[4]} |");
+            //Console.WriteLine($"6. {ismlar[5]} | {baholar[5]} |");
+            //Console.WriteLine($"7. {ismlar[6]} | {baholar[6]} |");
+            //Console.WriteLine($"8. {ismlar[7]} | {baholar[7]} |");
+            //Console.WriteLine($"9. {ismlar[8]} | {baholar[8]} |");
+            //Console.WriteLine($"10. {ismlar[9]} | {baholar[9]} |");
+
+            string ism = "";
+            int yosh = 0;
+            do
+            {
+                Console.WriteLine("Iltimos ma'lumotlarni kiriting");
+              ism = Console.ReadLine();
+              yosh = Convert.ToInt32(Console.ReadLine());
+            } while (String.IsNullOrEmpty(ism) && yosh > 6);
+            Console.WriteLine("Bu loop ni tashqarisida");
+
+            while (yosh < 18)
+            {
+                Console.WriteLine($"{ism} maktabda o'qiyapti va yoshi {yosh}da");
+                yosh++;
+            }
+            Console.WriteLine($"Demak {ism} 18 dan katta bo'lipti");
+
+
         }
     }
 }
---
+++ Loop [5-dars]/LoopPracticeSolution/LoopPractice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ism = "";
+            int yosh = 0;
+            string[] ismlar = new string[]
+            {
+                "Abduvaliev Sherzod",
+                "Bahriddinov Abdumajid",
+                "Doniyorov G'anisher",
+                "Nurbekov Bektemir",
+                "Nusratov Oybek",
+                "Mo'minova Amina",
+                "Eshonqulova Parvina",
+                "Qilichev Sardor",
+                "Qahramovon Amon",
+                "Teshaboyeb Temur"
+            };
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Iltimos ism va yoshni kiriting");
+                ism = Console.ReadLine();
+                yosh = Convert.ToInt32(Console.ReadLine());
+
+            } while (yosh < 6);
+
+            while (yosh < 10)
+            {
+                Console.WriteLine($"{ism} bizda o'qiyapti.\n Yoshi {yosh}");
+                yosh++;
+            }
+            Console.WriteLine("for dan foydalandik");
+            for (int i = 0; i < ismlar.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} {ismlar[i]}");
+            }
+            Console.WriteLine("foreach dan foydalandik");
+            foreach (var birIsm in ismlar)
+            {
+                Console.WriteLine($"{birIsm}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
